00dd7d4a300751f2ae7dc597aab1c5da4d3d87c3
René Kooi <rene@kooi.me>
2015-10-16 13:59:21 +0200

    set up babel compilation

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,9 @@
 import gulp from 'gulp';
+import { configure as babelify } from 'babelify';
+import browserify from 'browserify';
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
+import source from 'vinyl-source-stream';
 
 gulp.task('sass', () => {
     return gulp.src('./resources/assets/sass/app.scss')
@@ -8,6 +11,18 @@ gulp.task('sass', () => {
             .pipe(sass())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('public/style'));
+});
+
+gulp.task('js', () => {
+    const bundler = browserify({
+        debug: true,
+        entries: './resources/assets/js/app.js'
+    })
+    .transform(babelify({ stage: 0 }));
+
+    return bundler.bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('public/js'));
 });
 
 gulp.task('default', [ 'sass' ]);
