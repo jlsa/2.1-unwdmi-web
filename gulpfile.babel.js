420a37c4acdd24243401c56c20aab3a4a82e0978
René Kooi <rene@kooi.me>
2015-10-16 17:27:05 +0200

    Minify JS if `--minify` flag is passed

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,13 @@
 import gulp from 'gulp';
+import { env as args } from 'gulp-util';
 import { configure as babelify } from 'babelify';
 import browserify from 'browserify';
+import buffer from 'gulp-buffer';
+import uglify from 'gulp-uglify';
 import sass from 'gulp-sass';
 import sourcemaps from 'gulp-sourcemaps';
 import source from 'vinyl-source-stream';
+import when from 'gulp-if';
 
 gulp.task('sass', () => {
     return gulp.src('./resources/assets/sass/app.scss')
@@ -15,13 +19,15 @@ gulp.task('sass', () => {
 
 gulp.task('js', () => {
     const bundler = browserify({
-        debug: true,
+        debug: !args.minify,
         entries: './resources/assets/js/app.js'
     })
     .transform(babelify({ stage: 0 }));
 
     return bundler.bundle()
         .pipe(source('app.js'))
+        .pipe(when(args.minify, buffer()))
+        .pipe(when(args.minify, uglify()))
         .pipe(gulp.dest('public/js'));
 });
 
