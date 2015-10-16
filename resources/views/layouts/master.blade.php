8325ec29a82ac70726d82f05c0fb4f285c1a24d1
René Kooi <rene@kooi.me>
2015-10-16 14:11:50 +0200

    Add master layout view

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>UNWDMI</title>
+    <link rel="stylesheet" href="{{ asset('style/app.css') }}">
+</head>
+<body>
+    @yield('content')
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
