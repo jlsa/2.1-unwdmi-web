855b5ff615ec10a958ae65e876d0287623e526b7
Jari Germeraad <jarinoes@gmail.com>
2015-10-27 13:01:49 +0100

    Wrote query for the top 10 temperatures of last 24 hours at Kyoto's longitude.

--- app/Http/Controllers/Top10TemperatureController.php
+++ app/Http/Controllers/Top10TemperatureController.php
@@ -5,6 +5,8 @@ namespace Leertaak5\Http\Controllers;
 use Illuminate\Http\Request;
 use Leertaak5\Http\Requests;
 use Leertaak5\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Leertaak5\Measurement;
 
 class Top10TemperatureController extends Controller
 {
@@ -12,7 +14,18 @@ class Top10TemperatureController extends Controller
      * Shows the top 10 temperatures for the past 24 hours
      * within the longitude range of Kyoto.
      */
-    public function show() {
-        //Snippet
+    public function show()
+    {
+        //Grab all Measurements, where time is of the last
+        //24 hrs and the longitude = Kyoto's
+
+        //Kyoto's longitude is 135.733 in decimals
+        $longitude = 135.733;
+
+        $measurements = Measurement::where('longitude', $longitude)
+                        ->where('created_at', '>=', Carbon::now()->subDay())
+                        ->orderBy('temperate', 'desc')
+                        ->take(10)
+                        ->get();
     }
 }
