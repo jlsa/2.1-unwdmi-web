e3ab3f26f8f71f33d44f0eede896ec15a893ee1a
tieskedh <thijsdehaan71@hotmail.com>
2015-11-02 16:49:23 +0100

    add relation to station

--- app/Measurement.php
+++ app/Measurement.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Measurement extends Model
 {
-    //
+    public $timestamps = false;
+
+
+    public function station()
+    {
+        return $this->belongsTo('Leertaak5\Station', 'stn');
+    }
 }
