6c1e66d364fefcd0d3a4d82cda04153d9e7cc5f3
Jari Germeraad <jarinoes@gmail.com>
2015-11-03 12:23:54 +0100

    Finished with rainfall back-end. Renamed showPerStation to showMostRecent as that is the better way to describe its functionality. This is only applicable to the last commit, so please keep that in mind.

--- app/Http/Controllers/RainfallController.php
+++ app/Http/Controllers/RainfallController.php
@@ -7,6 +7,7 @@ use Leertaak5\Http\Requests;
 use Leertaak5\Http\Controllers\Controller;
 use Leertaak5\Station;
 use Leertaak5\Measurement;
+use Carbon\Carbon;
 
 /**
  * Returns an array, indexed by station numbers,
@@ -16,10 +17,38 @@ use Leertaak5\Measurement;
 class RainfallController extends Controller
 {
     /**
+     * Displays information per station of the last
+     * 24 hours.
+     */
+    public function showPerStation()
+    {
+        $data = array();
+
+        $stationid = 1;
+
+        $station = Station::where('id', '=', $stationid)
+        ->take(1)
+        ->get();
+
+        $data['station'] = $station;
+        $data['measurements'] = array();
+
+        $measurements = Measurement::with('station')
+        ->where('timestamp', '>=', Carbon::now()->subDay())
+        ->chunk(1000, function($measurements) use (&$data) {
+            foreach ($measurements as $measurement) {
+                $data['measurements'][] = $measurement;
+            }
+        });
+
+        return $data;
+    }
+
+    /**
      * Returns most recent longitude, latitude and precipation
      * per location.
      */
-    public function showPerStation()
+    public function showMostRecent()
     {
         $data = array();
 
