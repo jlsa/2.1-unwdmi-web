48bbe3ce9caa20b6440a0a7b766bb303fbbcff67
René Kooi <rene@kooi.me>
2015-11-03 13:13:07 +0100

    add station marker functionality to map

--- resources/assets/js/components/Map.js
+++ resources/assets/js/components/Map.js
@@ -1,5 +1,7 @@
+import assign from 'object-assign';
+import element from 'magic-virtual-element'; // eslint-disable-line no-unused-vars
 import leaflet from 'leaflet';
-import element from 'magic-virtual-element';
+import titleCase from 'title-case';
 
 const defaultProps = {
   width: 800,
@@ -8,15 +10,27 @@ const defaultProps = {
   zoom: 2
 };
 
-function render({ props, state }) {
+const stationIcon = leaflet.divIcon({
+  iconSize: leaflet.point(5, 5),
+  className: 'map-station'
+});
+
+function render({ props }) {
   return <div class="map" style={{ width: props.width, height: props.height }} />;
 }
 
 function afterMount({ props, state }, el, setState) {
-  const map = leaflet.map(el, props);
   const tiles = leaflet.tileLayer('https://maps.wikimedia.org/osm/{z}/{x}/{y}.png', {
     attribution: 'Map data ⓒ <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
   });
+  const markers = leaflet.layerGroup(
+    props.children.map(({ attributes }) => {
+      const { latitude, longitude, name, country } = attributes;
+      return leaflet.marker([ latitude, longitude ], { icon: stationIcon })
+        .bindPopup(`${titleCase(name)}, ${titleCase(country)}`);
+    })
+  );
+  const map = leaflet.map(el, assign({}, props, { layers: [ tiles, markers ] }));
   tiles.addTo(map);
   setState({ map });
 }
