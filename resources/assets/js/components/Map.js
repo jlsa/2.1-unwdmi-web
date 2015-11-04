21678b8ba349dd09a6fc2846a88d9c841e1ceed9
René Kooi <rene@kooi.me>
2015-11-04 12:33:41 +0100

    clusters!

--- resources/assets/js/components/Map.js
+++ resources/assets/js/components/Map.js
@@ -1,7 +1,9 @@
 import assign from 'object-assign';
 import element from 'magic-virtual-element'; // eslint-disable-line no-unused-vars
 import leaflet from 'leaflet';
+import 'leaflet.markercluster';
 import { render as renderTree, tree } from 'deku';
+import titleCase from 'title-case';
 import StationPopup from './Map/Station';
 
 const defaultProps = {
@@ -11,11 +13,6 @@ const defaultProps = {
   zoom: 2
 };
 
-const stationIcon = leaflet.divIcon({
-  iconSize: leaflet.point(5, 5),
-  className: 'map-station'
-});
-
 function render({ props }) {
   return <div class="map" style={{ width: props.width, height: props.height }} />;
 }
@@ -24,19 +21,42 @@ function afterMount({ props, state }, el, setState) {
   const tiles = leaflet.tileLayer('https://maps.wikimedia.org/osm/{z}/{x}/{y}.png', {
     attribution: 'Map data ⓒ <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
   });
-  const markers = leaflet.layerGroup(
-    props.children.map(({ attributes }) => {
-      const { id, latitude, longitude } = attributes;
-      const el = document.createElement('div');
-      return leaflet.marker([ latitude, longitude ], { icon: stationIcon })
-        .bindPopup(el)
-        .once('popupopen', e => {
-          // poopy! ):
-          renderTree(tree(<StationPopup id={id} popup={e.popup} />), el);
-        });
-    })
-  );
-  const map = leaflet.map(el, assign({}, props, { layers: [ tiles, markers ] }));
+  const markers = props.children.map(({ attributes }) => {
+    const { id, name, latitude, longitude } = attributes;
+    const el = document.createElement('div');
+    return leaflet.marker(
+      [ latitude, longitude ],
+      { icon: leaflet.divIcon({
+        iconSize: leaflet.point(17, 17),
+        className: 'map-marker map-marker--station map-marker--small',
+        html: titleCase(name)
+      }) }
+    )
+      .bindPopup(el)
+      .once('popupopen', e => {
+        // poopy! ):
+        renderTree(tree(<StationPopup id={id} popup={e.popup} />), el);
+      });
+  });
+  const sizes = {
+    small: leaflet.point(18, 18),
+    medium: leaflet.point(23, 23),
+    large: leaflet.point(28, 28)
+  };
+  const clusters = leaflet.markerClusterGroup({
+    maxClusterRadius: 40,
+    iconCreateFunction(cluster) {
+      const count = cluster.getChildCount();
+      const size = count < 10 ? 'small' : count < 30 ? 'medium' : 'large';
+      return leaflet.divIcon({
+        iconSize: sizes[size],
+        html: count,
+        className: `map-marker map-marker--cluster map-marker--${size}`
+      });
+    }
+  });
+  clusters.addLayers(markers);
+  const map = leaflet.map(el, assign({}, props, { layers: [ tiles, clusters ] }));
   setState({ map });
 }
 
