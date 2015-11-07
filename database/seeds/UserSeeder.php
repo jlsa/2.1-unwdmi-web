28565fffef8d16481aa9d64d43da79a3cda3d130
Marijn Pool <info@marijnpool.nl>
2015-11-07 19:33:05 +0100

    Added userseeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Leertaak5\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       User::create([
+           'email' => 'admin@admin.admin',
+           'name' => 'admin',
+           'password' => bcrypt('admin'),
+           'rights' => 1
+       ]);
+    }
+}
