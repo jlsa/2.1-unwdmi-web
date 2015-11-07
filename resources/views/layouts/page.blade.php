147972165b1752e2ccab8c440cdc8ebe0dcf1a3d
Marijn Pool <info@marijnpool.nl>
2015-11-07 20:57:42 +0100

    Improved login fancyness

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -2,6 +2,24 @@
 
 @section('master_content')
     <div class="container">
+        @if (session('status'))
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            </div>
+        </div>
+        @endif
+        @if (session('error'))
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-danger">
+                {{ session('error') }}
+                </div>
+            </div>
+        </div>
+        @endif
         @yield('content')
     </div>
 @endsection
