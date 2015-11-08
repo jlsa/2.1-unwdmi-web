3be90b6d31b74b4605f0804c2730d92971b17c09
Thijs de Haan <thijsdehaan71@hotmail.com>
2015-11-08 14:45:31 +0100

    fix for seeding
    Voorheen bleef de seeder hier hangen als je de stations al ingevuld had in verband met duplicated keys.

--- database/seeds/StationTableSeeder.php
+++ database/seeds/StationTableSeeder.php
@@ -11,6 +11,7 @@ class StationTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('stations')->truncate();
         $insertArray = array();
         $stations = file_get_contents(base_path().'/resources/stations.tsv');
         $stationArray = explode(PHP_EOL, $stations);
