d575020b8e031ca290a7d281fc0c23b007f322d5
Thijs de Haan <thijsdehaan71@hotmail.com>
2015-11-08 16:43:56 +0100

    verplaats errordiv naar juiste plek
    errordiv uit form gehaald

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,19 +8,18 @@
 </div>
 <div class="row">
     <div class="col-md-5">
+        @if($errors->count() > 0)
+            <div class="alert alert-danger" role="alert">
+                @foreach ($errors->all() as $error)
+                    <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                    <span class="sr-only">Error:</span>
+                    {{ $error }}<br />
+                @endforeach
+            </div>
+        @endif
+        
         <form method="POST" action('Auth\AuthController@getLogin')>
             {!! csrf_field() !!}
-            
-            @if($errors->count() > 0)
-                <div class="alert alert-danger" role="alert">
-                    @foreach ($errors->all() as $error)
-                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                        <span class="sr-only">Error:</span>
-                        {{ $error }}<br />
-                    @endforeach
-                </div>
-            @endif
-            
             <fieldset class="form-group">
                 <label for="email">Email</label>
                 <input class="form-control" type="email" name="email" value="{{ old('email') }}">
