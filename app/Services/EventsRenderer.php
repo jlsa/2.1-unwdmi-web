83062e0b57ee245f248945f771df2231f5ba90f6
René Kooi <rene@kooi.me>
2015-11-09 16:47:40 +0100

    avoid class constant

--- app/Services/EventsRenderer.php
+++ app/Services/EventsRenderer.php
@@ -3,10 +3,10 @@
 namespace Leertaak5\Services;
 
 class EventsRenderer {
-    const IMAGES = [ 'freeze', 'rain', 'snow', 'hail', 'thunder', 'cyclone' ];
+    private static $IMAGES = [ 'freeze', 'rain', 'snow', 'hail', 'thunder', 'cyclone' ];
     public function render($events)
     {
-        return collect(array_combine(self::IMAGES, str_split($events)))
+        return collect(array_combine(self::$IMAGES, str_split($events)))
             ->filter(function ($event) { return $event; })
             ->map(function ($event, $image) {
                 return '<img src="' . asset('img/' . $image . '.png') . '"
