a3b09eec5e0c80013166d37a69c243b14aa00116
Marijn Pool <info@marijnpool.nl>
2015-11-09 18:32:18 +0100

    Cleaned up routes file and added PagesController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,61 +11,49 @@
 |
 */
 
-Route::get('/', ['middleware' => 'auth', function () {
-    return view('welcome');
-}]);
+
 
 
 Route::group(['middleware' => 'auth'], function () {
-   Route::get('stations/{id}.json', 'StationsController@jsonShow');
-   Route::get('stations.json', 'StationsController@jsonIndex');
-   Route::get('stations/measurements/{id}', 'StationsController@showMeasurements');
-   Route::get('stations/{id}', 'StationsController@show');
-   Route::get('stations', 'StationsController@index');
 
+    Route::get('/', 'PagesController@index');
 
-   Route::get('measurements/{id}', 'MeasurementsController@show');
-   Route::get('measurements', 'MeasurementsController@index');
-   Route::get('kyoto-longitude', 'MeasurementsController@kyotoLongitude');
-   Route::get('temperatures', 'MeasurementsController@top10');
+    //Station routes
+    Route::get('stations/{id}.json', 'StationsController@jsonShow');
+    Route::get('stations.json', 'StationsController@jsonIndex');
+    Route::get('stations/measurements/{id}', 'StationsController@showMeasurements');
+    Route::get('stations/{id}', 'StationsController@show');
+    Route::get('stations', 'StationsController@index');
 
+    //Measurements routes
+    Route::get('measurements/{id}', 'MeasurementsController@show');
+    Route::get('measurements', 'MeasurementsController@index');
+    Route::get('kyoto-longitude', 'MeasurementsController@kyotoLongitude');
+    Route::get('temperatures', 'MeasurementsController@top10');
 
-   Route::get('world', 'RainfallController@index');
-   Route::get('measurements.json', 'RainfallController@showPerStation');
-   Route::get('rainmostrecent','RainfallController@showMostRecent');
+    //Rainfall map routes
+    Route::get('world', 'RainfallController@index');
+    Route::get('measurements.json', 'RainfallController@showPerStation');
+    Route::get('rainmostrecent','RainfallController@showMostRecent');
+
+    //Export routes
+    Route::get('download','DownloadController@index');
+
+    //Administrator panel routes...
+    Route::get('admin','Admin\AdminPanelController@show');
+    Route::get('admin/create_user','Admin\AdminPanelController@showCreateUser');
+    Route::get('admin/create','Admin\AdminPanelController@showUser');
+    Route::post('admin/create','Admin\AdminPanelController@createUser');
 });
 
 
 
 
 
-Route::get('download', [
-    'middleware' => 'auth',
-    'uses' => 'DownloadController@index'
-]);
+
 
 // Authentication routes...
 Route::get('login', 'Auth\AuthController@getLogin');
 Route::post('login', 'Auth\AuthController@postLogin');
 Route::get('logout', 'Auth\AuthController@getLogout');
 
-//Administrator panel routes...
-Route::get('admin', [
-    'middleware' => 'auth',
-    'uses' => 'Admin\AdminPanelController@show'
-]);
-
-Route::get('admin/create_user', [
-    'middleware' => 'auth',
-    'uses' => 'Admin\AdminPanelController@showCreateUser'
-]);
-
-Route::get('admin/create', [
-    'middleware' => 'auth',
-    'uses' => 'Admin\AdminPanelController@showUser'
-]);
-
-Route::post('admin/create', [
-    'middleware' => 'auth',
-    'uses' => 'Admin\AdminPanelController@createUser'
-]);
