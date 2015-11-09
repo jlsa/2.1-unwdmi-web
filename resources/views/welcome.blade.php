8fc6eaaab0017feafcef16b9d365a72795eb71a0
Marijn Pool <info@marijnpool.nl>
2015-11-10 00:50:37 +0100

    Removed double error log

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,5 @@
 @extends('layouts.page')
 
 @section('content')
-    @if (session('error'))
-        <div class="alert alert-danger">
-        {{ session('error') }}
-        </div>
-    @endif
     <div class="title">Welcome</div>
 @endsection
