bc2c2a6c070679d9e825e8354383ed555291aa6e
Marijn Pool <info@marijnpool.nl>
2015-11-09 10:13:39 +0100

    Thijs needs to learn to TEST things before pushing......

--- app/Http/Controllers/AllWeatherDataController.php
+++ app/Http/Controllers/AllWeatherDataController.php
@@ -22,10 +22,9 @@ class AllWeatherDataController extends Controller
         $longitude = 135.733;
 
         $measurements = Measurement::with('station')
-            ->whereIn('station_id', $stations->lists('id'))
             ->where('time', '>=', Carbon::now()->subWeek())
             ->where('temperature', '<', 20)
-            ->whereHas('stations', function ($sql) use ($longitude) {
+            ->whereHas('station', function ($sql) use ($longitude) {
                 $sql->where('longitude', '=', $longitude);
             })
             ->orderBy('time', 'desc');
