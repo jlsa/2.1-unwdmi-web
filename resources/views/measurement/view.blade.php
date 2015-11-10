5b5f2d50d25c210902e117ae2a6d0e91f804ddb0
jlsa <jlsahoekstra@gmail.com>
2015-11-10 09:04:49 +0000

    individual measurement table redesigned

--- resources/views/measurement/view.blade.php
+++ resources/views/measurement/view.blade.php
@@ -3,63 +3,46 @@
 @section('title','Measurement')
 
 @section('content')
-<table class="table">
-    <tr>
-        <td>station_id</td>
-        <td>
-            <a href="{{ action('StationsController@show', $measurement->station_id) }}">
-                {{ $measurement->station_id }}
-            </a>
-        </td>
-    </tr>
-    <tr>
-        <td>time</td>
-        <td>{{ $measurement->time }}</td>
-    </tr>
-    <tr>
-        <td>temperature</td>
-        <td>{{ $measurement->temperature }}</td>
-    </tr>
-    <tr>
-        <td>dew_point</td>
-        <td>{{ $measurement->dew_point }}</td>
-    </tr>
-    <tr>
-        <td>station_pressure</td>
-        <td>{{ $measurement->station_pressure }}</td>
-    </tr>
-    <tr>
-        <td>sea_level_pressure</td>
-        <td>{{ $measurement->sea_level_pressure }}</td>
-    </tr>
-    <tr>
-        <td>visibility</td>
-        <td>{{ $measurement->visibility }}</td>
-    </tr>
-    <tr>
-        <td>precipitation</td>
-        <td>{{ $measurement->precipitation }}</td>
-    </tr>
-    <tr>
-        <td>snow_depth</td>
-        <td>{{ $measurement->snow_depth }}</td>
-    </tr>
-    <tr>
-        <td>events</td>
-        <td>{{ $measurement->events }}</td>
-    </tr>
-    <tr>
-        <td>cloud_cover</td>
-        <td>{{ $measurement->cloud_cover }}</td>
-    </tr>
-    <tr>
-        <td>wind_direction</td>
-        <td>{{ $measurement->wind_direction }}</td>
-    </tr>
-    <tr>
-        <td>wind_speed</td>
-        <td>{{ $measurement->wind_speed }}</td>
-    </tr>
+<table class="table table-bordered table-striped">
+    <thead class="header_rotate">
+        <tr>
+            <th><div>Station Id</div></th>
+            <th><div>Time</div></th>
+            <th><div>Temperature</div></th>
+            <th><div>Dew Point</div></th>
+            <th><div>Station Pressure</div></th>
+            <th><div>Sea Level Pressure</div></th>
+            <th><div>Visibility</div></th>
+            <th><div>Precipitation</div></th>
+            <th><div>Snow Depth</div></th>
+            <th><div>Events</div></th>
+            <th><div>Cloud Cover</div></th>
+            <th><div>Wind Direction</div></th>
+            <th><div>Wind Speed</div></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                <a href="{{ action('StationsController@show', $measurement->station_id) }}">
+                    {{ $measurement->station_id }}
+                </a>
+            </td>
+            <td>{{ $measurement->time }}</td>
+            <td>{{ $measurement->temperature }}</td>
+            <td>{{ $measurement->dew_point }}</td>
+            <td>{{ $measurement->station_pressure }}</td>
+            <td>{{ $measurement->sea_level_pressure }}</td>
+            <td>{{ $measurement->visibility }}</td>
+            <td>{{ $measurement->precipitation }}</td>
+            <td>{{ $measurement->snow_depth }}</td>
+            <td>{{ $measurement->events }}</td>
+            <td>{{ $measurement->cloud_cover }}</td>
+            <td>{{ $measurement->wind_direction }}</td>
+            <td>{{ $measurement->wind_speed }}</td>
+        </tr>
+
+    </tbody>
 </table>
 
 @endsection
